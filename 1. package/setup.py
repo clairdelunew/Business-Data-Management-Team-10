9177181da57ac240bdd1ddc4e3296d7835313456
王陕煜 <wang@wangshanyudeMacBook-Air.local>
2025-02-18 16:12:25 +0100

    更新了数据清理代码和README

--- 1. package/setup.py
+++ 1. package/setup.py
@@ -1,17 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
-setup(
-    name="my_spider",  # 项目名称
-    version="1.0.0",  # 版本号
-    packages=find_packages(),  # 自动发现 Python 包
-    install_requires=[line.strip() for line in open("requirements.txt").readlines()],  # 自动读取 requirements.txt
-    entry_points={
-        "console_scripts": [
-            "my_spider=my_spider.scrape:main"  # 让 my_spider 命令运行 scrape.py 里的 main() 函数
-        ]
-    },
-    author="group10",
-    author_email="your_email@example.com",
-    description="A web crawler to scrape Panerai watch data.",
-    python_requires=">=3.10.14",
-)
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='luxury',
+      version="0.0.7",
+      description="Luxury-project (cloud_training)",
+      license="MIT",
+      author="EDHEC",
+      author_email="juandavid.casascano@edhec.com",
+      install_requires=requirements,
+      packages=find_packages(),
+    #   test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      zip_safe=False)
